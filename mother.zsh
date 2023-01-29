0272411263fd58179576f445d87236c1466d5c15
SuperKenVery <39673849+SuperKenVery@users.noreply.github.com>
2023-01-29 16:13:09 +0800

    Update mother.zsh
    fuck more things

--- mother.zsh
+++ mother.zsh
@@ -1,5 +1,12 @@
 #! /usr/bin/env zsh
 
-set -e
+# If we failed to do anything we should keep going
+# set -e
+
 
 rm -rf ~/.zshrc
+rm -rf ~/.zprofile
+rm -rf ~/.zcompdump*
+rm -rf ~/.zshenv
+rm -rf .oh-my-zsh
+rm -rf .ohmyzsh
